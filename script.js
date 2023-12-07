c1a30d6dbd6bc917053d28bc4e76e45e50b61394
pylova <pylova.di@gmail.com>
2023-12-07 09:28:33 +0000

    shoppind card added

--- script.js
+++ script.js
@@ -17,3 +17,63 @@ document.addEventListener("DOMContentLoaded", function () {
 
     }
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    const addToCartButtons = document.querySelectorAll(".add-to-cart");
+    const cartItems = document.getElementById("cart-items");
+    const cartTotal = document.getElementById("cart-total");
+
+    addToCartButtons.forEach(button => {
+        button.addEventListener("click", addToCart);
+    });
+
+    // Load cart items from local storage
+    loadCart();
+
+    function addToCart(event) {
+        const ticket = event.target.parentElement;
+        const ticketId = ticket.getAttribute("data-id");
+        const ticketName = ticket.querySelector("h2").textContent;
+        const ticketPrice = parseFloat(ticket.querySelector("p").textContent.slice(8));
+
+        // Create a new cart item
+        const cartItem = document.createElement("li");
+        cartItem.innerHTML = `${ticketName} - $${ticketPrice}`;
+        cartItems.appendChild(cartItem);
+
+        // Update the cart total
+        const currentTotal = parseFloat(cartTotal.textContent);
+        cartTotal.textContent = (currentTotal + ticketPrice).toFixed(2);
+
+        // Save the cart to local storage
+        saveCart(ticketId);
+    }
+  
+
+    function saveCart(itemId) {
+        let cart = JSON.parse(localStorage.getItem("cart")) || {};
+        cart[itemId] = (cart[itemId] || 0) + 1;
+        localStorage.setItem("cart", JSON.stringify(cart));
+    }
+
+    function loadCart() {
+        const cart = JSON.parse(localStorage.getItem("cart"));
+        if (cart) {
+            for (let itemId in cart) {
+                const ticket = document.querySelector(`[data-id="${itemId}"]`);
+                const ticketName = ticket.querySelector("h2").textContent;
+                const ticketPrice = parseFloat(ticket.querySelector("p").textContent.slice(8));
+                const quantity = cart[itemId];
+
+                // Create a new cart item
+                const cartItem = document.createElement("li");
+                cartItem.innerHTML = `${ticketName} - $${ticketPrice} x ${quantity}`;
+                cartItems.appendChild(cartItem);
+
+                // Update the cart total
+                const currentTotal = parseFloat(cartTotal.textContent);
+                cartTotal.textContent = (currentTotal + ticketPrice * quantity).toFixed(2);
+            }
+        }
+    }
+});
